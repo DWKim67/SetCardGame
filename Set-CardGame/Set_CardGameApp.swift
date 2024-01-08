7dc3fd7998e558e712b7b986a1a5338606b398fa
DWKim67 <dkim67@my.yorku.ca>
2024-01-08 13:04:53 -0500

    Initial Commit

---
+++ Set-CardGame/Set_CardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Set_CardGameApp.swift
+//  Set-CardGame
+//
+//  Created by Daniel Kim on 2024-01-08.
+//
+
+import SwiftUI
+
+@main
+struct Set_CardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

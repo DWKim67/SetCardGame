4d91a2f2a3354be2cae980c4d3cf986059b8ee86
DWKim67 <dkim67@my.yorku.ca>
2024-01-16 17:21:07 -0500

    Finished A3, started A4

---
+++ Set-CardGame/CardContentView.swift
@@ -0,0 +1,18 @@
+//
+//  CardContentView.swift
+//  Set-CardGame
+//
+//  Created by Daniel Kim on 2024-01-12.
+//
+
+import SwiftUI
+
+struct CardContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardContentView()
+}

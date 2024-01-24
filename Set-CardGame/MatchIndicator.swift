5107306b64aed8086487c30f03f94e11b4015765
DWKim67 <dkim67@my.yorku.ca>
2024-01-24 11:37:24 -0500

    Roughly finished A4, fixing/refactoring needed

---
+++ Set-CardGame/MatchIndicator.swift
@@ -0,0 +1,18 @@
+//
+//  MatchIndicator.swift
+//  Set-CardGame
+//
+//  Created by Daniel Kim on 2024-01-22.
+//
+
+import SwiftUI
+
+struct MatchIndicator: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MatchIndicator()
+}
---
+++ Set-CardGame/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Set-CardGame
+//
+//  Created by Daniel Kim on 2024-01-22.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}

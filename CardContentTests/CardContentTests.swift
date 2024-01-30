0d89257022d2fc7d4b610ec8c1e8d04ab7654c10
DWKim67 <dkim67@my.yorku.ca>
2024-01-29 20:41:12 -0500

    pt 2

--- CardContentTests/CardContentTests.swift
+++ CardContentTests/CardContentTests.swift
@@ -17,19 +17,13 @@ final class CardContentTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testFullEquality() throws {
+        let cardContent1 = CardContent(color: "blue", number: 1, shape: "Circle", shading: "Opaque")
+        let cardContent2 = CardContent(color: "blue", number: 1, shape: "Circle", shading: "Opaque")
+        
+        XCTAssertTrue(cardContent1 == cardContent2)
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+    
 
 }
